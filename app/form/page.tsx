94fe5032f29ab0af377e938b2e5299d6863b9f59
Vovarama1992 <vovayhh9988@gmail.com>
2024-05-18 18:45:17 +0300

    suspense

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -2,7 +2,7 @@
 import styles from '../page.module.scss';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import Link from 'next/link';
-import { useState, ChangeEvent } from 'react';
+import { useState, ChangeEvent, Suspense } from 'react';
 
 const text = `Я прочитал(а), ознакомился(лась) и согласен(на) с Политикой конфиденциальности, Согласием на обработку персональных данных физическим лицом. Я осознаю, что даю Согласие на обработку персональных данных для использования предоставляемых возможностей Веб-сервиса.`;
 
@@ -57,70 +57,72 @@ export default function Form() {
   }
   
   return (
-    <form onSubmit={handleSubmit} className={styles.form}>
-      <div className={styles.fillTitle}></div>
-      <div className={styles.credentials}>
-        <div className={styles.inputWrapper}>
-          <input
-            className={styles.input}
-            value={surname}
-            onChange={(e) => changeCreds(e.target.value, 0)}
-            placeholder="Ф.И.О"
-          ></input>
+    <Suspense fallback={<div>Loading...</div>}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <div className={styles.fillTitle}></div>
+        <div className={styles.credentials}>
+          <div className={styles.inputWrapper}>
+            <input
+              className={styles.input}
+              value={surname}
+              onChange={(e) => changeCreds(e.target.value, 0)}
+              placeholder="Ф.И.О"
+            ></input>
+          </div>
+          <div className={styles.inputWrapper}>
+            <input
+              className={styles.input}
+              type='tel'
+              value={tel}
+              onChange={(e) => changeCreds(e.target.value, 1)}
+              placeholder="Телефон"
+            ></input>
+          </div>
         </div>
-        <div className={styles.inputWrapper}>
-          <input
-            className={styles.input}
-            type='tel'
-            value={tel}
-            onChange={(e) => changeCreds(e.target.value, 1)}
-            placeholder="Телефон"
-          ></input>
+        <div className={styles.response}>Отклик на вакансию</div>
+        <div className={styles.selectBlock}>
+          Выберите вакансию
+          <select
+            name="vacancy"
+            onChange={(e) => setOption(e.target.value)}
+            className={styles.select}
+          >
+            <option value="прораб">прораб</option>
+            <option value="специалист службы снабжения">специалист службы снабжения</option>
+            <option value="логист">логист</option>
+            <option value="проектировщик">проектировщик</option>
+          </select>
         </div>
-      </div>
-      <div className={styles.response}>Отклик на вакансию</div>
-      <div className={styles.selectBlock}>
-        Выберите вакансию
-        <select
-          name="vacancy"
-          onChange={(e) => setOption(e.target.value)}
-          className={styles.select}
-        >
-          <option value="прораб">прораб</option>
-          <option value="специалист службы снабжения">специалист службы снабжения</option>
-          <option value="логист">логист</option>
-          <option value="проектировщик">проектировщик</option>
-        </select>
-      </div>
-      <div className={styles.resume}>Ваше резюме</div>
-      <div className={styles.ifPdf}>
-        Есть резюме в формате .pdf или .doc? Приложите и отправьте его нам.
-      </div>
-      <button type="button" className={styles.loader} onClick={loader}>
-        Загрузить файл
-      </button>
-      <input
-        type="file"
-        id="fileInput"
-        style={{ display: 'none' }}
-        onChange={handleFileChange}
-      />
-      <div className={styles.confirmer}>
+        <div className={styles.resume}>Ваше резюме</div>
+        <div className={styles.ifPdf}>
+          Есть резюме в формате .pdf или .doc? Приложите и отправьте его нам.
+        </div>
+        <button type="button" className={styles.loader} onClick={loader}>
+          Загрузить файл
+        </button>
         <input
-          type="checkbox"
-          checked={confirmed}
-          onChange={() => setConfirm(!confirmed)}
-          className={styles.checkbox}
-          name="confirmer"
-        ></input>
-        {text}
-        <br />
-        <br />
-        Я достиг 18-летнего возраста
-      </div>
-      <button type="submit" disabled={!conditionToSend} className={styles.sender}>
-        Отправить анкету
-      </button>
-    </form>
+          type="file"
+          id="fileInput"
+          style={{ display: 'none' }}
+          onChange={handleFileChange}
+        />
+        <div className={styles.confirmer}>
+          <input
+            type="checkbox"
+            checked={confirmed}
+            onChange={() => setConfirm(!confirmed)}
+            className={styles.checkbox}
+            name="confirmer"
+          ></input>
+          {text}
+          <br />
+          <br />
+          Я достиг 18-летнего возраста
+        </div>
+        <button type="submit" disabled={!conditionToSend} className={styles.sender}>
+          Отправить анкету
+        </button>
+      </form>
+    </Suspense>
   );
 }
--- app/sendedForm/page.tsx
+++ app/sendedForm/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import styles from '../page.module.scss';
+import { Suspense } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
 export default function SendedForm() {
     const searchParams = useSearchParams();
@@ -14,22 +15,24 @@ export default function SendedForm() {
     const text = `Благодарим за ваш отклик и интерес.`;
 
     return (
-        <div className={styles.form}>
-           <div className={styles.declaration}>
-           Анкета на должность {vacancy} отправлена.
-           Поздравляю, {surname}
-           </div>
-           <div className={styles.checkMark}>
-            <div className={styles.innerCheck}></div>
-            {text}
-            <br></br>
-            <br></br>
-            Мы изучим вашу анкету и свяжемся с вами в nечении 2х дней.
+        <Suspense fallback={<div>Loading...</div>}>
+            <div className={styles.form}>
+               <div className={styles.declaration}>
+               Анкета на должность {vacancy} отправлена.
+               Поздравляю, {surname}
+               </div>
+               <div className={styles.checkMark}>
+                <div className={styles.innerCheck}></div>
+                {text}
+                <br></br>
+                <br></br>
+                Мы изучим вашу анкету и свяжемся с вами в nечении 2х дней.
+                </div>
+               <button onClick={backer} className={styles.returnButton}>
+                На главную
+               </button>
             </div>
-           <button onClick={backer} className={styles.returnButton}>
-            На главную
-           </button>
-        </div>
+        </Suspense>
 
     )
 }
